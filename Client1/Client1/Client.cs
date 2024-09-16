78ac6db8271233aadad7f457861e98077c9f2a2b
Aniluffi <90216018+Aniluffi@users.noreply.github.com>
2024-09-16 11:44:54 +0300

    Add files via upload

---
+++ Client1/Client1/Client.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client1
+{
+    class Client
+    {
+        /// <summary>
+        /// поле для отмены ввода сообщения
+        /// </summary>
+        CancellationTokenSource _stop = new CancellationTokenSource();
+        /// <summary>
+        /// буфер для хранения сообщений
+        /// </summary>
+        List<string> _BufferMessege = new List<string>();
+
+        /// <summary>
+        /// метод для поиска сервера
+        /// </summary>
+        /// <param name="iP">айпи сервера</param>
+        /// <param name="port">порт</param>
+        /// <returns></returns>
+        public async Task SearchServer(IPAddress iP,int port)
+        {
+            using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                await tcpClient.ConnectAsync(iP, port);
+
+                Task.Run(async () => SendMessege(tcpClient), _stop.Token);
+
+                await Task.Run(async () => await AcceptMessege(tcpClient));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// метод для принятия сообщений
+        /// </summary>
+        /// <param name="tcpClient">клиент</param>
+        /// <returns></returns>
+        private async Task AcceptMessege(Socket tcpClient)
+        {
+            while (true)
+            {
+                if (tcpClient.Poll(10, SelectMode.SelectRead))
+                {
+                    var buf = new byte[1024];
+                    int acceptBytes = await tcpClient.ReceiveAsync(buf);
+
+                    string acceptWord = Encoding.UTF8.GetString(buf, 0, acceptBytes);
+
+                    _stop.Cancel();
+
+                    UpdayteChat(acceptWord);
+
+                    Console.WriteLine("Send:");
+                }
+            }
+        }
+
+        /// <summary>
+        /// метод для обновления чата
+        /// </summary>
+        /// <param name="saveUp">сообщения на сохранение</param>
+        private void UpdayteChat(string saveUp)
+        {
+            Console.Clear();
+            _BufferMessege.Add(saveUp);
+
+            Console.WriteLine(ToString());
+        }
+
+        /// <summary>
+        /// преобразование в строку сообщений
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string messege = "";
+            foreach (var item in _BufferMessege) messege += item + "\n";
+            return messege;
+        }
+
+        /// <summary>
+        /// метод для отправки сообщений
+        /// </summary>
+        /// <param name="tcpClient"></param>
+        /// <returns></returns>
+        private async Task SendMessege(Socket tcpClient)
+        {
+            while (true)
+            {
+                Console.WriteLine("Send:");
+                string word = Console.ReadLine();
+                byte[] data = Encoding.UTF8.GetBytes(word);
+
+                await tcpClient.SendAsync(data);
+
+                UpdayteChat("Я: " + word + "\n");
+            }
+        }
+    }
+}
+
---
+++ Client1/Client1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Client1;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+
+
+class Program
+{
+    /// <summary>
+    /// точка входа
+    /// </summary>
+    /// <returns></returns>
+    public static async Task Main()
+    {
+       Client client = new Client();
+        await client.SearchServer(IPAddress.Parse("192.168.1.135"), 8888);
+    }
+
+}
